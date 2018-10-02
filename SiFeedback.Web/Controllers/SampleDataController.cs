070cb6bfad1eb8882cb19ef894f3d5175f6afe5f
SOFTINVEST\Andrii.Solovienko <andrii.solovienko@uapay.ua>
2018-10-02 18:22:55 +0300

    rm cmt

--- SiFeedback.Web/Controllers/SampleDataController.cs
+++ SiFeedback.Web/Controllers/SampleDataController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SiFeedback.Web.Controllers
 {
-    //Comment
+ 
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
